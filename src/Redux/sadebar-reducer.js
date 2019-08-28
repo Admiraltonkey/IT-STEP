4e31cefc338cc785ef61d7f6754746e8df08ec2e
Admiraltonkey <AdmiralTonkey@gmail.com>
2019-08-28 17:48:17 +0300

    41 step

---
+++ src/Redux/sadebar-reducer.js
@@ -0,0 +1,5 @@
+const sadebarReducer = (state,action)=>{
+
+    return state;
+}
+export default sadebarReducer;
--- src/componen/Dialogs/Dialogs.js
+++ src/componen/Dialogs/Dialogs.js
@@ -3,7 +3,7 @@ import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 import PropTypes from 'prop-types';
-import {sendMessageCreator, updateNewMessageBodyCreator} from "../../Redux/State";
+import {sendMessageCreator, updateNewMessageBodyCreator} from "../../Redux/dialogs-reducer";
 
 const Dialogs = (props) => {
     let state = props.store.getState().dialogsPage;
@@ -16,6 +16,7 @@ const Dialogs = (props) => {
     let onSendMessageClick  = ()=>{
         props.store.dispatch(sendMessageCreator())
 }
+// 888
 let onNewMessageChange = (e)=>{
     let body = e.target.value;
     props.store.dispatch(updateNewMessageBodyCreator(body))
